9e87f03a82110cb390d5be8237314671bad1a712
Calel <thaisla_renner@hotmail.com>
2023-03-07 10:47:47 -0400

    criando componentes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ThemeProvider } from 'styled-components';
+import { Loading} from '@components/Loading';
 import { Groups } from '@screens/Groups';
+import { StatusBar } from 'react-native';
+
+import theme from './src/theme';
 
 export default function App() {
-  return (
-    <Groups />
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { Container } from './styles';
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
-  );
-}
+    <Container>
+      <Header/>
+      <Highlight 
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <GroupCard title='Galera do Ignite' />
+    </Container>
+  );
+};
